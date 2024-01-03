85aa15600628b60500dd2dca3431d97228059c77
David Picard <picard@ensea.fr>
2024-01-03 14:41:21 +0100

    improve pos interpolation + add option to not use input conv (if input is already tokenized)

--- src/model/vision.py
+++ src/model/vision.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 from .layers import HoMLayer
 
 class HoMVision(nn.Module):
-    def __init__(self, nb_classes, dim=256, im_size=256, kernel_size=16, nb_layers=12, order=4, order_expand=8, ffw_expand=4, dropout=0.):
+    def __init__(self, nb_classes, dim=256, im_size=256, kernel_size=16, nb_layers=12, order=4, order_expand=8, ffw_expand=4, dropout=0., pooling='cls', in_conv=True):
         super().__init__()
         self.nb_classes = nb_classes
         self.dim = dim
@@ -14,8 +14,11 @@ class HoMVision(nn.Module):
         self.order = order
         self.order_expand = order_expand
         self.ffw_expand = ffw_expand
+        self.pooling = pooling
 
-        self.conv = nn.Conv2d(3, dim, kernel_size=kernel_size, stride=kernel_size)
+        self.conv = None
+        if in_conv:
+            self.conv = nn.Conv2d(3, dim, kernel_size=kernel_size, stride=kernel_size)
         self.layers = nn.ModuleList([HoMLayer(dim, order, order_expand, ffw_expand, dropout=dropout) for i in range(nb_layers)])
         self.out_proj = nn.Linear(dim, nb_classes)
 
@@ -32,7 +35,10 @@ class HoMVision(nn.Module):
         # init
         self.apply(self.init_weights_)
         nn.init.zeros_(self.out_proj.weight)
-        nn.init.constant_(self.out_proj.bias, -6.9)
+        if pooling == 'cls':
+            nn.init.constant_(self.out_proj.bias, -6.9)
+        else:
+            nn.init.zeros_(self.out_proj.bias)
 
 
     def init_weights_(self, m):
@@ -41,26 +47,35 @@ class HoMVision(nn.Module):
             if m.bias is not None:
                 nn.init.zeros_(m.bias)
 
-    def forward(self, x):
+    def forward(self, x, mask=None):
 
-        x = self.conv(x)
-        x = einops.rearrange(x, 'b c h w -> b (h w) c')
+        if self.conv is not None:
+            x = self.conv(x)
+            x = einops.rearrange(x, 'b c h w -> b (h w) c')
 
         b, n, c = x.shape
         ones = torch.ones(b, 1, 1).to(x.device)
         if n == self.position.shape[1]:
             position = self.position
         else:
-            position = nn.functional.interpolate(self.position.unsqueeze(0), size=(n,c), mode='bicubic').squeeze(0)
+            position = einops.rearrange(self.position, "b (m n) d ->b d m n", m=self.im_size//self.kernel_size)
+            m = torch.sqrt(torch.tensor(n)).int()
+            position = nn.functional.interpolate(position, size=(m, m), mode='bicubic')
+            position = einops.rearrange(position, "b d m n -> b (m n) d")
         x = x + position * ones
         cls = self.cls * ones
 
         x = torch.cat([cls, x], dim=1)
+        if mask is not None:
+            mask = torch.cat([torch.ones(b, 1).to(mask.device), mask], dim=1)
 
         for i in range(self.nb_layers):
-            x = self.layers[i](x)
+            x = self.layers[i](x, mask)
 
-        x = self.out_proj(x[:, 0, :])
+        if self.pooling == 'cls':
+            x = self.out_proj(x[:, 0, :])
+        else:
+            x = self.out_proj(x)[:, 1:, :] # return without cls
 
         return x
 
