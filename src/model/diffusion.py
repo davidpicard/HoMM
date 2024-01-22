49a1a39b457c53619ab5e4504794ffa9582fd5f1
David Picard <picard@ensea.fr>
2024-01-22 15:40:53 +0100

    fix logging and create dedicated training script with better default

--- src/model/diffusion.py
+++ src/model/diffusion.py
@@ -1,8 +1,12 @@
 
 import pytorch_lightning as L
 import torch
+from torchvision.transforms import transforms
 
-
+denormalize = transforms.Normalize(
+    mean=[-0.485 / 0.229, -0.456 / 0.224, -0.406 / 0.225],
+    std=[1.0 / 0.229, 1.0 / 0.224, 1.0 / 0.225],
+)
 class DiffusionModule(L.LightningModule):
     def __init__(
             self,
@@ -11,7 +15,7 @@ class DiffusionModule(L.LightningModule):
             loss,
             optimizer_cfg,
             lr_scheduler_builder,
-            train_batch_preprocess,
+            # train_batch_preprocess,
             val_sampler,
         ):
         super().__init__()
@@ -20,12 +24,11 @@ class DiffusionModule(L.LightningModule):
         self.loss = loss
         self.optimizer_cfg = optimizer_cfg
         self.lr_scheduler_builder = lr_scheduler_builder
-        self.train_batch_preprocess = train_batch_preprocess
+        # self.train_batch_preprocess = train_batch_preprocess
         self.val_sampler = val_sampler
 
     def training_step(self, batch, batch_idx):
         img, label = batch
-        img, label = self.train_batch_preprocess(img, label)
         b, c, h, w = img.shape
 
         #sample time, noise, make noisy
@@ -35,21 +38,45 @@ class DiffusionModule(L.LightningModule):
 
         pred = self.model(img, label, time)
         loss = self.loss(pred, eps, average=True)
+        for metric_name, metric_value in loss.items():
+            self.log(
+                f"train/{metric_name}",
+                metric_value,
+                sync_dist=True,
+                on_step=True,
+                on_epoch=True,
+            )
         return loss
 
     def validation_step(self, batch, batch_idx):
-        self.log("val/loss", 0., sync_dist=True, on_step=False, on_epoch=True)
+        img, label = batch
+        b, c, h, w = img.shape
+        #sample time, noise, make noisy
+        time = torch.rand(b).to(img.device)
+        eps = torch.randn_like(img)
+        img = torch.sqrt(1-time).reshape(b, 1, 1, 1) * img + torch.sqrt(time).reshape(b, 1, 1, 1) * eps
+        pred = self.model(img, label, time)
+        loss = self.loss(pred, eps, average=True)
+        for metric_name, metric_value in loss.items():
+            self.log(
+                f"val/{metric_name}",
+                metric_value,
+                sync_dist=True,
+                on_step=True,
+                on_epoch=True,
+            )
 
-        # noise = torch.randn_like(img)
-        # samples = self.val_sampler.sample(noise, self.model, label)
-        # for img, lbl in zip(samples, label):
-        #     self.log_image(
-        #         "val/img_{lbl}",
-        #         img,
-        #         sync_dist=True,
-        #         on_step=True,
-        #         on_epoch=True,
-        #     )
+        # sample images
+        noise = torch.randn_like(img)
+        samples = self.val_sampler.sample(noise, self.model, label)
+        samples = denormalize(samples)
+        label = label.argmax(dim=1)
+        self.logger.log_image(
+            key="samples",
+            images=[samples[0], samples[1], samples[3], samples[4]],
+            caption=[label[1], label[2], label[3], label[4]]
+        )
+
     def configure_optimizers(self):
         if self.optimizer_cfg.exclude_ln_and_biases_from_weight_decay:
             parameters_names_wd = get_parameter_names(self.model, [nn.LayerNorm])
