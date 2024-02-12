7a295999eb4b24452e7b32c240144df5b6c7899b
David Picard <picard@ensea.fr>
2024-02-12 08:13:46 +0100

    now that dit is fixed, working on HoMM version, DiH.

--- src/model/network/imagediffusion.py
+++ src/model/network/imagediffusion.py
@@ -3,7 +3,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from .layers import HoMLayer
+from .layers import HoM
 from timm.models.vision_transformer import Attention
 
 
@@ -45,8 +45,6 @@ class DiTBlock(nn.Module):
 
         return x
 
-from diffusers.models.transformer_2d import Transformer2DModelOutput
-
 class ClassConditionalDiT(nn.Module):
     def __init__(self,
                  n_classes: int,
@@ -54,9 +52,8 @@ class ClassConditionalDiT(nn.Module):
                  im_size: int,
                  kernel_size: int,
                  dim: int,
-                 n_layers,
-                 order=2,
-                 order_expand=4,
+                 n_layers: int,
+                 n_heads: int,
                  ffw_expand=4,
                  dropout=0.):
         super().__init__()
@@ -66,8 +63,7 @@ class ClassConditionalDiT(nn.Module):
         self.kernel_size = kernel_size
         self.dim = dim
         self.n_layers = n_layers
-        self.order = order
-        self.order_expand = order_expand
+        self.n_heads = n_heads
         self.ffw_expand = ffw_expand
         self.dropout = dropout
 
@@ -84,7 +80,7 @@ class ClassConditionalDiT(nn.Module):
         self.pos_emb = nn.Parameter(torch.zeros((1, self.n_patches ** 2, dim)), requires_grad=False)
         self.in_conv = nn.Conv2d(3, dim, kernel_size=kernel_size, stride=kernel_size, bias=True)
         self.layers = nn.ModuleList(
-            [DiTBlock(dim, order) for _ in range(n_layers)])
+            [DiTBlock(dim, n_heads) for _ in range(n_layers)])
         self.out_ln = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
         self.out_proj = nn.Linear(dim, kernel_size * kernel_size * 3, bias=True)
         self.out_mod = nn.Sequential(
@@ -121,9 +117,7 @@ class ClassConditionalDiT(nn.Module):
         nn.init.zeros_(self.out_mod[-1].weight)
         nn.init.zeros_(self.out_mod[-1].bias)
 
-
-
-    def forward(self, img, time, cls, return_dict: bool = True,):
+    def forward(self, img, time, cls):
 
         # patchify
         x = self.in_conv(img)
@@ -153,7 +147,92 @@ class ClassConditionalDiT(nn.Module):
         return out
 
 
-class ClassConditionalHoMDiffusion(nn.Module):
+def DiT_XL_2(**kwargs):
+    return ClassConditionalDiT(n_layers=28, dim=1152, kernel_size=2, n_heads=16, **kwargs)
+
+def DiT_XL_4(**kwargs):
+    return ClassConditionalDiT(n_layers=28, dim=1152, kernel_size=4, n_heads=16, **kwargs)
+
+def DiT_XL_8(**kwargs):
+    return ClassConditionalDiT(n_layers=28, dim=1152, kernel_size=8, n_heads=16, **kwargs)
+
+def DiT_L_2(**kwargs):
+    return ClassConditionalDiT(n_layers=24, dim=1024, kernel_size=2, n_heads=16, **kwargs)
+
+def DiT_L_4(**kwargs):
+    return ClassConditionalDiT(n_layers=24, dim=1024, kernel_size=4, n_heads=16, **kwargs)
+
+def DiT_L_8(**kwargs):
+    return ClassConditionalDiT(n_layers=24, dim=1024, kernel_size=8, n_heads=16, **kwargs)
+
+def DiT_B_2(**kwargs):
+    return ClassConditionalDiT(n_layers=12, dim=768, kernel_size=2, n_heads=12, **kwargs)
+
+def DiT_B_4(**kwargs):
+    return ClassConditionalDiT(n_layers=12, dim=768, kernel_size=4, n_heads=12, **kwargs)
+
+def DiT_B_8(**kwargs):
+    return ClassConditionalDiT(n_layers=12, dim=768, kernel_size=8, n_heads=12, **kwargs)
+
+def DiT_S_2(**kwargs):
+    return ClassConditionalDiT(n_layers=12, dim=384, kernel_size=2, n_heads=6, **kwargs)
+
+def DiT_S_4(**kwargs):
+    return ClassConditionalDiT(n_layers=12, dim=384, kernel_size=4, n_heads=6, **kwargs)
+
+def DiT_S_8(**kwargs):
+    return ClassConditionalDiT(n_layers=12, dim=384, kernel_size=8, n_heads=6, **kwargs)
+
+
+DiT_models = {
+    'DiT-XL/2': DiT_XL_2,  'DiT-XL/4': DiT_XL_4,  'DiT-XL/8': DiT_XL_8,
+    'DiT-L/2':  DiT_L_2,   'DiT-L/4':  DiT_L_4,   'DiT-L/8':  DiT_L_8,
+    'DiT-B/2':  DiT_B_2,   'DiT-B/4':  DiT_B_4,   'DiT-B/8':  DiT_B_8,
+    'DiT-S/2':  DiT_S_2,   'DiT-S/4':  DiT_S_4,   'DiT-S/8':  DiT_S_8,
+}
+
+
+
+###############################
+# DiH
+
+class DiHBlock(nn.Module):
+    def __init__(self, dim: int, order: int, order_expand: int, ffw_expand: int):
+        super().__init__()
+        self.dim = dim
+        self.order = order
+        self.order_expand = order_expand
+        self.ffw_expand = ffw_expand
+
+        self.mha_ln = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
+        self.hom = HoM(dim, order=order, order_expand=order_expand, bias=True)
+        self.ffw_ln = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
+        self.ffw = nn.Sequential(nn.Linear(dim, ffw_expand * dim, bias=True),
+                                 nn.GELU(approximate="tanh"),
+                                 nn.Linear(ffw_expand * dim, dim, bias=True))
+        self.cond_mlp = nn.Sequential(
+                                 nn.SiLU(),
+                                 nn.Linear(dim, 4 * dim, bias=True))
+        self.gate_mlp = nn.Sequential(
+                                 nn.SiLU(),
+                                 nn.Linear(dim, 2 * dim, bias=True))
+
+
+    def forward(self, x, c):
+        s1, b1, s2, b2 = self.cond_mlp(c).chunk(4, -1)
+        g1, g2 = self.gate_mlp(c).chunk(2, -1)
+
+        # mha
+        x_ln = modulation(self.mha_ln(x), s1.unsqueeze(1), b1.unsqueeze(1))
+        x = x + self.hom(x_ln)*(1+g1.unsqueeze(1))
+
+        #ffw
+        x_ln = modulation(self.ffw_ln(x), s2.unsqueeze(1), b2.unsqueeze(1))
+        x = x + self.ffw(x_ln)*(1+g2.unsqueeze(1))
+
+        return x
+
+class ClassConditionalDiH(nn.Module):
     def __init__(self,
                  n_classes: int,
                  n_timesteps: int,
@@ -177,84 +256,84 @@ class ClassConditionalHoMDiffusion(nn.Module):
         self.ffw_expand = ffw_expand
         self.dropout = dropout
 
-        self.classes_emb = nn.Parameter(torch.zeros((n_layers, n_classes, dim)), requires_grad=True)
-        self.time_emb = nn.Parameter(torch.zeros((n_layers, n_timesteps+1, dim)), requires_grad=True)
-
-        self.n_patches = (im_size//kernel_size)
-        self.pos_emb = nn.Parameter(torch.zeros((1, self.n_patches**2, dim)))
-        # self.n_registers = 16
-        # self.register = nn.Parameter(torch.zeros(1, self.n_registers, dim), requires_grad=True)
-
-        self.in_conv = nn.Conv2d(3, dim, kernel_size=kernel_size, stride=kernel_size)
-        # self.sa_layers = nn.ModuleList([HoMLayer(dim, order, order_expand, ffw_expand, dropout) for i in range(n_layers)])
-        # self.ca_layers = nn.ModuleList(
-        #     [HoMLayer(dim, order, order_expand, ffw_expand, dropout) for i in range(n_layers)])
-        self.sa_layers = nn.ModuleList(
-            [DiTBlock(dim, n_classes, order, n_timesteps) for i in range(n_layers)])
-        # self.ca_layers = nn.ModuleList(
-        #     [AttentionModule(dim, order, order_expand, ffw_expand, dropout) for i in range(n_layers)])
-        self.out_ln = nn.LayerNorm(dim)
-        self.outproj = nn.Linear(dim, kernel_size*kernel_size*3)
+        self.classes_emb = nn.Embedding(n_classes + 1, dim)
+        self.freqs = nn.Parameter(torch.exp(-2 * np.log(n_timesteps) * torch.arange(0, dim//2) / dim), requires_grad=False)
+        self.time_emb = nn.Sequential(nn.Linear(dim, 4*dim, bias=True),
+                                      nn.SiLU(),
+                                      nn.Linear(4*dim, dim, bias=True)
+                                      )
+        self.n_patches = (im_size // kernel_size)
+        # for diffusers
+        self.in_channels = 3
+        self.sample_size = (self.n_patches, self.n_patches)
+        self.pos_emb = nn.Parameter(torch.zeros((1, self.n_patches ** 2, dim)), requires_grad=False)
+        self.in_conv = nn.Conv2d(3, dim, kernel_size=kernel_size, stride=kernel_size, bias=True)
+        self.layers = nn.ModuleList(
+            [DiHBlock(dim=dim, order=order, order_expand=order_expand, ffw_expand=ffw_expand) for _ in range(n_layers)])
+        self.out_ln = nn.LayerNorm(dim, elementwise_affine=False, eps=1e-6)
+        self.out_proj = nn.Linear(dim, kernel_size * kernel_size * 3, bias=True)
+        self.out_mod = nn.Sequential(
+            nn.SiLU(),
+            nn.Linear(dim, 2 * dim, bias=True)
+        )
 
         # init
-        nn.init.trunc_normal_(
-            self.classes_emb, std=0.02, a=-2 * 0.02, b=2 * 0.02
-        )
-        nn.init.trunc_normal_(
-            self.time_emb, std=0.02, a=-2 * 0.02, b=2 * 0.02
-        )
-        nn.init.trunc_normal_(
-            self.pos_emb, std=0.02, a=-2 * 0.02, b=2 * 0.02
-        )
-        # nn.init.trunc_normal_(
-        #     self.register, std=0.02, a=-2 * 0.02, b=2 * 0.02
-        # )
-        self.apply(self.init_weights_)
+        # layers
+        def init_weights_(m):
+            if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
+                nn.init.xavier_uniform_(m.weight)
+                if m.bias is not None:
+                    nn.init.zeros_(m.bias)
+        self.apply(init_weights_)
+        # zeros modulation gates
+        for l in self.layers:
+            m = l.cond_mlp[-1]
+            nn.init.zeros_(m.weight)
+            nn.init.zeros_(m.bias)
+            m = l.gate_mlp[-1]
+            nn.init.zeros_(m.weight)
+            nn.init.constant_(m.bias, -1.)
+        #pos emb
+        pos_emb = get_2d_sincos_pos_embed(self.pos_emb.shape[-1], self.n_patches)
+        self.pos_emb.data.copy_(torch.from_numpy(pos_emb).float().unsqueeze(0))
+        # patch and time emb
+        nn.init.normal_(self.classes_emb.weight, std=0.02)
+        nn.init.normal_(self.time_emb[0].weight, std=0.02)
+        nn.init.normal_(self.time_emb[2].weight, std=0.02)
+        # output
+        nn.init.zeros_(self.out_proj.weight)
+        nn.init.zeros_(self.out_proj.bias)
+        nn.init.zeros_(self.out_mod[-1].weight)
+        nn.init.zeros_(self.out_mod[-1].bias)
 
-    def init_weights_(self, m):
-        if isinstance(m, nn.Linear) or isinstance(m, nn.Conv2d):
-            nn.init.xavier_uniform_(m.weight)
-            if m.bias is not None:
-                nn.init.zeros_(m.bias)
+    def forward(self, img, time, cls):
 
-    def forward(self, img, cls, time):
-        # quantize time
-        time = time * self.n_timesteps
-        tq = torch.clamp(time.floor().long(), 0, self.n_timesteps)
-        t2 = (time - tq).unsqueeze(1)
-        t1 = 1 - t2
-
-        #patchify
+        # patchify
         x = self.in_conv(img)
         x = einops.rearrange(x, 'b d h w -> b (h w) d')
         b, n, d = x.shape
         x = x + self.pos_emb * torch.ones((b, 1, 1)).to(x.device)
 
-        # registers
-        # r = self.register * torch.ones((b, 1, 1)).to(x.device)
+        # embed time
+        time = torch.einsum("b, n -> bn", time, self.freqs)
+        t = torch.cat([time.cos(), time.sin()], dim=1)
+        t = self.time_emb(t)
+        # cond
+        c = self.classes_emb(cls)
+        c = c+t
 
         # forward pass
         for l in range(self.n_layers):
-            #CA
-            c = self.classes_emb[l, cls.argmax(dim=1)]
-            t = (t1*self.time_emb[l, tq, ...] + t2*self.time_emb[l, tq+1, ...])
-            # ctx = torch.cat([t, c], dim=1)
-            # r = self.ca_layers[l](r, xc=ctx) # registers read from ctx
-            # sa
-            # x = torch.cat([r, x], dim=1)
-            # x = self.sa_layers[l](x) # both r and x read from everything
-            # x = x[:,self.n_registers:, :]
-            # r = x[:, :self.n_registers, :]
-
-            # dit
-            x = self.sa_layers[l](x, c, t)
+            x = self.layers[l](x, c)
+        s, b = self.out_mod(c).chunk(2, dim=-1)
+        out = modulation(self.out_ln(x), s.unsqueeze(1), b.unsqueeze(1))
+        out = self.out_proj(out)
 
         # depatchify
-        out = self.outproj(self.out_ln(x))
         out = einops.rearrange(out, 'b (h w) (k s c) -> b c (h k) (w s)',
                                h=self.n_patches, k=self.kernel_size, c=3)
 
-        return (out,)
+        return out
 
 
 # https://github.com/facebookresearch/mae/blob/main/util/pos_embed.py
--- src/model/network/layers.py
+++ src/model/network/layers.py
@@ -2,6 +2,47 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
+class HoM(nn.Module):
+    def __init__(self, dim, order, order_expand, bias=True):
+        super().__init__()
+        self.dim = dim
+        self.order = order
+        self.order_expand = order_expand
+
+        self.ho_proj = nn.Linear(dim, order*order_expand*dim, bias=bias)
+        self.se_proj = nn.Linear(dim, order*order_expand*dim, bias=bias)
+        self.ag_proj = nn.Linear(order*order_expand*dim, dim, bias=bias)
+
+    def forward(self, xq, xc=None, mask=None):
+        if xc is None:
+            xc = xq # self attention
+
+        # high order
+        h = self.ho_proj(xc)
+        h = list(h.chunk(self.order, dim=-1))
+        for i in range(1, self.order):
+            h[i] = h[i] * F.tanh(h[i-1])
+        h = torch.cat(h, dim=-1)
+        # averaging
+        if mask is None:
+            h = h.mean(dim=1, keepdims=True)
+        else:
+            if mask.dim()==2:
+                h = (h * mask.unsqueeze(-1)).sum(dim=1, keepdims=True)/mask.unsqueeze(-1).sum(dim=1, keepdims=True)
+            elif mask.dim() ==3:
+                h = torch.einsum(h, mask, 'bnd, bmn -> bmd')/mask.sum(dim=2, keepdims=True) # b batch, n context tokens, m query tokens, d dim
+            else:
+                raise Exception('unsupported dim for mask (should be 2 or None)')
+
+        # selection
+        s = F.sigmoid(self.se_proj(xq))
+        sh = s * h
+
+        # aggregation
+        return self.ag_proj(sh)
+
+
 class HoMLayer(nn.Module):
     def __init__(self, dim, order, order_expand, ffw_expand, dropout=0.):
         super().__init__()
@@ -36,7 +77,7 @@ class HoMLayer(nn.Module):
             if mask.dim()==2:
                 h = (h * mask.unsqueeze(-1)).sum(dim=1, keepdims=True)/mask.unsqueeze(-1).sum(dim=1, keepdims=True)
             elif mask.dim() ==3:
-                h = torch.einsum(h, mask, 'bnd, bmn -> bmd') # b batch, n context tokens, m query tokens, d dim
+                h = torch.einsum(h, mask, 'bnd, bmn -> bmd')/mask.sum(dim=2, keepdims=True) # b batch, n context tokens, m query tokens, d dim
             else:
                 raise Exception('unsupported dim for mask (should be 2 or None)')
 
