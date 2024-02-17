76b488964e452863fba764c007d558ac5740d517
David Picard <picard@ensea.fr>
2024-02-17 10:37:23 +0100

    remove stupid broken checkpoint strategy from lightning... bis repetita

--- src/train_diffusion.py
+++ src/train_diffusion.py
@@ -72,7 +72,7 @@ def train(cfg):
         print('loading weights from {}'.format(cfg.load_weight_from_checkpoint))
         sd = torch.load(cfg.load_weight_from_checkpoint)
         state_dict = sd['state_dict']
-        module_state_dict = {}
+        module_state_dict = state_dict
         # for key, value in state_dict.items():
         #     if key.startswith('model._orig_mod.'):
         #         new_key = key[len('model._orig_mod.'):]
