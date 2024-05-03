e9bd550208a5560cd93d47bd7fc5f30b35bff51b
David Picard <picard@ensea.fr>
2024-05-03 20:10:00 +0200

    trying EulerScheduler

--- src/model/sampler/sampler.py
+++ src/model/sampler/sampler.py
@@ -19,7 +19,7 @@ def sigmoid_schedule(t):
 
 def karras_schedule(t):
     sigma_min = 1e-2
-    sigma_max = 80
+    sigma_max = 2
     rho = 7.
     min_inv_rho = sigma_min ** (1 / rho)
     max_inv_rho = sigma_max ** (1 / rho)
