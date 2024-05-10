c8e169d189b486ac8f02476da506f9f4b75086cb
David Picard <picard@ensea.fr>
2024-05-11 01:12:28 +0200

    -upscale: dix dimension bug

--- src/upscale_checkpoint.py
+++ src/upscale_checkpoint.py
@@ -43,14 +43,14 @@ ckpt['state_dict']['ema.ema_model.pos_emb'] = pos_up
 if dihpp:
     cond_pos = model.cond_pos_emb
     print('expanding conditional pos emb (old shape: {})'.format(cond_pos.shape))
-    cond_pos_r = cond_pos[0:model.n_registers]
+    cond_pos_r = cond_pos[:, 0:model.n_registers, :]
     print('found {} registers'.format(model.n_registers))
-    pos = cond_pos[model.n_registers:, ...]
+    pos = cond_pos[:, model.n_registers:, :]
     print('cond pos emb after removing registers: {}'.format(pos.shape))
     pos = einops.rearrange(pos, "b (h w) d -> b d h w", h=args.size//8)
     pos_up = torch.nn.functional.interpolate(pos, scale_factor=scale, mode='nearest')
     pos_up = einops.rearrange(pos_up, "b d h w -> b (h w) d")
-    cond_pos_up = torch.cat([cond_pos_r, pos_up], dim=0)
+    cond_pos_up = torch.cat([cond_pos_r, pos_up], dim=1)
     print('done (new shape: {})'.format(cond_pos_up.shape))
 
     ckpt['state_dict']['model.cond_pos_emb'] = cond_pos_up
