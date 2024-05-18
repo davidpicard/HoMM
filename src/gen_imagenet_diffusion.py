f11717efb04784e1a220dd4f55d6186e40f80d2b
David Picard <picard@ensea.fr>
2024-05-18 09:10:27 +0200

    diff: - add model names to generation

--- src/gen_imagenet_diffusion.py
+++ src/gen_imagenet_diffusion.py
@@ -48,15 +48,15 @@ elif precision_type == "fp16":
 print("loading model")
 if args.model_name == "DiHpp-S/2":
     print("model: DiHpp-S/2")
-    model = DiHpp_models["DiHpp-S/2"]
+    model = DiHpp_models["DiHpp-S/2"](input_dim=4, n_classes=1000, im_size=args.size//8)
 elif args.model_name == "DiHpp-B/2":
-    model = DiHpp_models["DiHpp-B/2"]
+    model = DiHpp_models["DiHpp-B/2"](input_dim=4, n_classes=1000, im_size=args.size//8)
     print("model: DiHpp-B/2")
 elif args.model_name == "DiHpp-L/2":
-    model = DiHpp_models["DiHpp-L/2"]
+    model = DiHpp_models["DiHpp-L/2"](input_dim=4, n_classes=1000, im_size=args.size//8)
     print("model: DiHpp-L/2")
 elif args.model_name == "DiHpp-XL/2":
-    model = DiHpp_models["DiHpp-XL/2"]
+    model = DiHpp_models["DiHpp-XL/2"](input_dim=4, n_classes=1000, im_size=args.size//8)
     print("model: DiHpp-XL/2")
 else:
     model = ClassConditionalDiHpp(input_dim=4,
