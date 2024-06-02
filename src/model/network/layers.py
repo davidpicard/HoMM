0e86254c7a01eea7dc32ad618dceb0e79c21b1a6
David Picard <picard@ensea.fr>
2024-06-02 23:58:50 +0200

    base: - rework layer to have consistent use of functions

--- src/model/network/layers.py
+++ src/model/network/layers.py
@@ -59,10 +59,7 @@ class HoMLayer(nn.Module):
         self.order_expand = order_expand
         self.ffw_expand = ffw_expand
 
-        self.ho_proj = nn.Linear(dim, order*order_expand*dim)
-        self.se_proj = nn.Linear(dim, order*order_expand*dim)
-        self.ag_proj = nn.Linear(order*order_expand*dim, dim)
-        self.ho_drop = nn.Dropout(p=dropout)
+        self.hom = HoM(self.dim, self.order, self.order_expand)
         self.ffw = nn.Sequential(nn.Dropout(p=dropout),
                                  nn.Linear(dim, ffw_expand*dim),
                                  nn.GELU(),
@@ -73,28 +70,7 @@ class HoMLayer(nn.Module):
             xc = xq # self attention
 
         # high order
-        h = F.tanh(self.ho_proj(self.ho_drop(xc)))
-        h = list(h.chunk(self.order, dim=-1))
-        for i in range(1, self.order):
-            h[i] = h[i] * h[i-1]
-        h = torch.cat(h, dim=-1)
-        # averaging
-        if mask is None:
-            h = h.mean(dim=1, keepdims=True)
-        else:
-            if mask.dim()==2:
-                h = (h * mask.unsqueeze(-1)).sum(dim=1, keepdims=True)/mask.unsqueeze(-1).sum(dim=1, keepdims=True)
-            elif mask.dim() ==3:
-                h = torch.einsum(h, mask, 'bnd, bmn -> bmd')/mask.sum(dim=2, keepdims=True) # b batch, n context tokens, m query tokens, d dim
-            else:
-                raise Exception('unsupported dim for mask (should be 2 or None)')
-
-        # selection
-        s = F.sigmoid(self.se_proj(xq))
-        sh = s * h
-
-        # aggregation
-        x = xq + self.ag_proj(sh)
+        x = xq + self.hom(xq, xc, mask)
 
         # ffw
         x = x + self.ffw(x)
