ee7dfcd2b0645e1c98d366967debe076f9057548
David Picard <picard@ensea.fr>
2024-08-24 16:35:44 +0200

    diff: - adding a Tar imagenet dataset with creation and loading

---
+++ src/create_imagenet_tar.py
@@ -0,0 +1,134 @@
+import json
+import tarfile
+import numpy as np
+import torch
+from diffusers import AutoencoderKL
+from torchvision import transforms
+from argparse import ArgumentParser
+from torchvision.datasets import ImageNet
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+import io
+
+parser = ArgumentParser()
+parser.add_argument("--imagenet-path", type=str, required=True)
+parser.add_argument("--output", type=str, required=True)
+
+parser.add_argument("--device", type=str, default="cuda")
+parser.add_argument("--precision", type=str, default="bf16")
+parser.add_argument("--batch-size", type=int, default=4)
+parser.add_argument("--workers", type=int, default=8)
+parser.add_argument("--size", type=int, default=512)
+parser.add_argument("--quantization-scale", type=float, default=1.5)
+parser.add_argument("--chunk-size", type=int, default=10000)
+args = parser.parse_args()
+
+
+precision_type = torch.float
+if args.precision == "bf16":
+    precision_type = torch.bfloat16
+elif precision_type == "fp16":
+    precision_type = torch.float16
+
+## imagenet loader
+tr = [
+    transforms.Resize(int(args.size)),
+    transforms.CenterCrop(args.size),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=0.5, std=0.5),
+]
+transform_train = transforms.Compose(tr)
+
+train = ImageNet(
+    args.imagenet_path,
+    transform=transform_train,
+)
+train = DataLoader(train, batch_size=args.batch_size, shuffle=True, num_workers=args.workers)
+
+## vae
+vae = AutoencoderKL.from_pretrained("runwayml/stable-diffusion-v1-5", device="cuda:0", subfolder="vae",
+                                                 use_safetensors=True)
+vae = vae.to(args.device)
+vae.eval()
+for p in vae.parameters():
+    p.requires_grad = False
+
+
+class TarWriter():
+    def __init__(self, dirname, chunk_size=10000, quantization_scale=1.5):
+        import os, io, tarfile, json
+        self.dir = dirname
+        self.chunks_size = chunk_size
+        self.quantization_scale = quantization_scale
+        try:
+            os.makedirs(dirname, exist_ok=True)
+        except:
+            print(f"Impossible to create {dirname}")
+
+        self.filelist = []
+        self.current_tar = None
+        self.current_filename = None
+        self.current_sample_count = 0
+
+    def check_chunk_size(self):
+        if (self.current_tar is None) or (self.current_sample_count >= self.chunks_size):
+            self.add_tarfile()
+
+    def add_tarfile(self):
+        self.close()
+        self.current_filename = f"{self.dir}/chunk_{len(self.filelist)}.tar"
+        self.current_tar = tarfile.open(self.current_filename, "w")
+        self.current_sample_count = 0
+        print(f"*** open {self.current_filename}")
+
+    def close(self):
+        if self.current_tar is not None:
+            self.current_tar.close()
+            self.filelist.append({"filename":self.current_filename,
+                                  "count":self.current_sample_count,
+                                  "quantization_scale": self.quantization_scale})
+            with open(f"{self.dir}/index.json", "w") as f:
+                json.dump(self.filelist, f)
+            self.current_tar = None
+            self.current_filename = None
+            print(f"*** closed all files")
+
+    def add_sample(self, name, buffer):
+        self.check_chunk_size()
+        info = tarfile.TarInfo(name)
+        info.size =  buffer.getbuffer().nbytes
+        self.current_tar.addfile(info, buffer)
+        self.current_sample_count += 1
+
+out = TarWriter(args.output, chunk_size=args.chunk_size, quantization_scale = args.quantization_scale)
+count = 0
+max_m = 0
+max_s = 0
+for img, lbl in tqdm(train):
+    img = img.to(args.device)
+    imgf = img.flip(dims=(3,))
+    img = torch.cat([img, imgf], dim=0)
+    lbl = torch.cat([lbl, lbl])
+    with torch.autocast(device_type=args.device, dtype=precision_type, enabled=True):
+        dist = vae.encode(img).latent_dist
+        latent_mean = dist.mean
+        latent_std = dist.std
+    max_m = latent_mean.abs().max() if latent_mean.abs().max() > max_m else max_m
+    max_s = latent_std.abs().max() if latent_std.abs().max() > max_s else max_s
+    latent_meanu8 = (latent_mean * args.quantization_scale).clamp(-127, 127).to(torch.int8)
+    latent_stdu8 = (latent_std * 1000* args.quantization_scale).clamp(-127, 127).to(torch.int8)
+
+    for i in range(len(lbl)):
+        name = f"sample_{count}.npz"
+        buffer = io.BytesIO()
+        np.savez_compressed(buffer, latent_meanu8[i, ...].cpu().numpy(), latent_stdu8[i, ...].cpu().numpy(), lbl[i].cpu().numpy())
+        buffer.seek(0)
+        out.add_sample(name, buffer)
+        count += 1
+        if count >= 25000:
+            break
+    if count >= 25000:
+        break
+out.close()
+print(f"Finished with max_m: {max_m} max_s: {max_s}")
+
---
+++ src/data/TarDataset.py
@@ -0,0 +1,122 @@
+import torch
+from torch.utils.data import Dataset
+import json
+import tarfile
+import io
+import numpy as np
+from collections import OrderedDict
+import threading
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cache = OrderedDict()
+        self.capacity = capacity
+
+    def __del__(self):
+        if len(self.cache) > 0:
+            key, tar = self.cache.popitem(last=False)
+            tar.close()
+
+    def get(self, key: int) -> tarfile.TarFile:
+        if key not in self.cache:
+            return -1
+        else:
+            self.cache.move_to_end(key)
+            return self.cache[key]
+
+    def put(self, key: int, value: tarfile.TarFile) -> None:
+        if key in self.cache:
+            self.cache.move_to_end(key)
+        self.cache[key] = value
+        if len(self.cache) > self.capacity:
+            key, tar = self.cache.popitem(last=False)
+            tar.close()
+
+
+def build_imagenet_tar(data_dir):
+    train = TarDataset(data_dir)
+    val = TarDataset(data_dir)
+    return train, val
+
+
+class TarDataset(Dataset):
+    def __init__(self, dirname, seed=3407):
+        self.dir = dirname
+        self.filenames = []
+        self.counts = []
+        self.q = []
+        with open(f"{self.dir}/index.json", "r") as f:
+            j = json.load(f)
+            for d in j:
+                self.filenames.append(d['filename'])
+                self.counts.append(d['count'])
+                self.q.append(d['quantization_scale'])
+        self.total_count = sum(self.counts)
+        assert self.total_count > 0
+        self.transform = None
+        self.generator = torch.Generator().manual_seed(seed)
+
+        self.cache = LRUCache(capacity=80)
+        self.lock = threading.Lock()
+
+    def __len__(self):
+        return self.total_count
+
+    def __getitem__(self, idx):
+        assert idx < self.total_count
+        current_file_id = 0
+        while idx >= self.counts[current_file_id]:
+            idx -= self.counts[current_file_id]
+            current_file_id += 1
+
+        # cehck cache
+        with self.lock:
+            tar = self.cache.get(current_file_id)
+            if tar == -1:
+                tar = tarfile.open(self.filenames[current_file_id], 'r')
+                self.cache.put(current_file_id, tar)
+
+        members = tar.getmembers()
+        member = members[idx]
+        f = tar.extractfile(member)
+        data = np.load(io.BytesIO(f.read()))
+        m = torch.from_numpy(data['arr_0']/self.q[current_file_id]).float()
+        s = torch.from_numpy(data['arr_1']/(1000*self.q[current_file_id])).float()
+        l = torch.nn.functional.one_hot(torch.from_numpy(data['arr_2']), num_classes=1000)
+        return m + s * torch.randn(m.shape, generator=self.generator), l
+
+if __name__ == "__main__":
+    import argparse
+    from torch.utils.data import DataLoader
+    import torch
+    import matplotlib.pyplot as plt
+    import einops
+    from diffusers import AutoencoderKL
+    vae = AutoencoderKL.from_pretrained("runwayml/stable-diffusion-v1-5", device="cuda:0", subfolder="vae",
+                                                     use_safetensors=True)
+    vae = vae.to("cuda")
+    vae.eval()
+    for p in vae.parameters():
+        p.requires_grad = False
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", type=str, required=True)
+    args = parser.parse_args()
+
+    train = TarDataset(args.dir)
+    train = DataLoader(train, batch_size=4, shuffle=True, num_workers=2)
+
+    plt.ion()
+
+    for samples, l in train:
+
+        samples = samples.to("cuda")
+        with torch.autocast(device_type="cuda", dtype=torch.float, enabled=True):
+            dec = vae.decode(samples).sample
+
+        imgs = dec.clamp(-1., 1.)*0.5+0.5
+        plt.imshow(einops.rearrange(imgs.cpu(), "(k b) c h w -> (k h) (b w) c", k=2))
+        plt.title(str(l))
+        plt.show()
+        plt.pause(0.1)
