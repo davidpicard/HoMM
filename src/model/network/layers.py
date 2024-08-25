77cdd264c5a0ce9803e8a938d9fc9ce32585cf04
David Picard <picard@ensea.fr>
2024-08-25 19:19:24 +0200

    HoM: base module optimization

--- src/model/network/layers.py
+++ src/model/network/layers.py
@@ -3,24 +3,64 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
+@torch.compile
+def gelu(x: torch.Tensor):
+    return x * torch.erf(x)
+
+@torch.compile
+def po2(x: torch.Tensor):
+    h1, h2 = gelu(x).chunk(2, dim=-1)
+    h2 = h2 * h1
+    return torch.cat([h1, h2], dim=-1)
+
+@torch.compile
+def po3(x: torch.Tensor):
+    h1, h2, h3 = gelu(x).chunk(3, dim=-1)
+    h2 = h2 * h1
+    h3 = h3 * h2
+    return torch.cat([h1, h2, h3], dim=-1)
+
+@torch.compile
+def po4(x: torch.Tensor):
+    h1, h2, h3, h4 = gelu(x).chunk(4, dim=-1)
+    h2 = h2 * h1
+    h3 = h3 * h2
+    h4 = h4 * h3
+    return torch.cat([h1, h2, h3, h4], dim=-1)
+
 def high_order_aggregation_(x: torch.Tensor, k: int, mask=None):
-        h = list(F.gelu(x).chunk(k, dim=-1))
+    if k == 2:
+        h = po2(x)
+    elif k == 3:
+        h = po3(x)
+    elif k == 4:
+        h = po4(x)
+    else:
+        h = list(gelu(x).chunk(k, dim=-1))
         for i in range(1, k):
             h[i] = h[i] * h[i-1]
         h = torch.cat(h, dim=-1)
-        if mask is None:
-            h = h.mean(dim=1, keepdims=True)
+    if mask is None:
+        h = h.mean(dim=1, keepdims=True)
+    else:
+        if mask.dim()==2:
+            h = (h * mask.unsqueeze(-1)).sum(dim=1, keepdims=True)/mask.unsqueeze(-1).sum(dim=1, keepdims=True)
+        elif mask.dim() ==3:
+            mask = mask.type(h.dtype)
+            h = torch.einsum('bnd, bmn -> bmd', h, mask)  # b batch, n context tokens, m query tokens, d dim
+            h = h/(1+mask.sum(dim=2, keepdims=True))
         else:
-            if mask.dim()==2:
-                h = (h * mask.unsqueeze(-1)).sum(dim=1, keepdims=True)/mask.unsqueeze(-1).sum(dim=1, keepdims=True)
-            elif mask.dim() ==3:
-                mask = mask.type(h.dtype)
-                h = torch.einsum('bnd, bmn -> bmd', h, mask)  # b batch, n context tokens, m query tokens, d dim
-                h = h/(1+mask.sum(dim=2, keepdims=True))
-            else:
-                raise Exception('unsupported dim for mask (should be 2,3 or None)')
-        return h
+            raise Exception('unsupported dim for mask (should be 2,3 or None)')
+    return h
 
+@torch.compile
+def high_order_selection_(x: torch.Tensor, h: torch.Tensor):
+    return F.sigmoid(x) * h
+
+def hom(xq: torch.Tensor, xc: torch.Tensor, k: int, mask=None):
+    h = high_order_aggregation_(xc, k, mask)
+    o = high_order_selection_(xq, h)
+    return o
 
 class HoM(nn.Module):
     def __init__(self, dim, order, order_expand, bias=True):
@@ -34,18 +74,17 @@ class HoM(nn.Module):
         self.ag_proj = nn.Linear(order*order_expand*dim, dim, bias=bias)
 
         #self.high_order_aggregation_ = torch.compile(high_order_aggregation_, mode="max-autotune", fullgraph=False)
-        self.high_order_aggregation_ = high_order_aggregation_
+        # self.high_order_aggregation_ = high_order_aggregation_
+        # self.high_order_selection_ = high_order_selection_
+        self.hom = hom
 
     def forward(self, xq, xc=None, mask=None):
         if xc is None:
             xc = xq # self attention
 
-        # high order
-        h = self.high_order_aggregation_(self.ho_proj(xc), self.order, mask)
-
-        # selection
-        s = F.sigmoid(self.se_proj(xq))
-        sh = s * h
+        s = self.se_proj(xq)
+        h = self.ho_proj(xc)
+        sh = self.hom(s, h, self.order, mask)
 
         # aggregation
         return self.ag_proj(sh)
