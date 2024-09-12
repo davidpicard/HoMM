8358727126f964abf0f995df2bc423b63a19987a
David Picard <picard@ensea.fr>
2024-09-12 23:03:10 +0200

    tardata: better split of files in sampler

--- src/data/TarDataset.py
+++ src/data/TarDataset.py
@@ -42,6 +42,53 @@ def build_imagenet_tar(data_dir):
     return train, val
 
 
+from torch.utils.data import Sampler
+import torch.distributed as dist
+import math
+
+class TarDistributedSampler(Sampler):
+    def __init__(self, dataset, num_replicas=None, rank=None, shuffle=True, seed=0):
+        if num_replicas is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            num_replicas = dist.get_world_size()
+        if rank is None:
+            if not dist.is_available():
+                raise RuntimeError("Requires distributed package to be available")
+            rank = dist.get_rank()
+        self.dataset = dataset
+        self.num_replicas = num_replicas
+        self.rank = rank
+        self.epoch = 0
+        self.num_samples = int(math.ceil(len(self.dataset) * 1.0 / self.num_replicas))
+        self.total_size = self.num_samples * self.num_replicas
+        self.shuffle = shuffle
+        self.seed = seed
+
+    def __iter__(self):
+        indices = list(range(len(self.dataset)))
+
+        # Add extra samples to make it evenly divisible
+        indices += indices[:(self.total_size - len(indices))]
+        assert len(indices) == self.total_size
+
+        # Divide indices into contiguous chunks
+        chunk_size = self.total_size // self.num_replicas
+        start = self.rank * chunk_size
+        end = start + chunk_size
+        indices = indices[start:end]
+        assert len(indices) == self.num_samples
+        # print(f"start: {start} chunk_size: {chunk_size}")
+
+        return iter(indices)
+
+    def __len__(self):
+        return self.num_samples
+
+    def set_epoch(self, epoch):
+        self.epoch = epoch
+
+
 class TarDataset(Dataset):
     def __init__(self, dirname, seed=3407):
         self.dir = dirname
@@ -66,16 +113,19 @@ class TarDataset(Dataset):
         return self.total_count
 
     def __getitem__(self, idx):
-        assert idx < self.total_count
-        # current_file_id = 0
-        # while idx >= self.counts[current_file_id]:
-        #     idx -= self.counts[current_file_id]
-        #     current_file_id += 1
-        current_file_id = idx % len(self.filenames)
-        idx = idx // len(self.filenames)
-        # print(f"pid: {os.getpid()} fid: {current_file_id} idx: {idx}")
+        id = idx
 
-        # cehck cache
+        assert idx < self.total_count
+        current_file_id = 0
+        while idx >= self.counts[current_file_id]:
+            idx -= self.counts[current_file_id]
+            current_file_id += 1
+
+        # current_file_id = idx % len(self.filenames)
+        # idx = idx // len(self.filenames)
+        # print(f"pid: {os.getpid()}  id: {id} fid: {current_file_id} fidx: {idx}")
+
+        # check cache
         with self.lock:
             tar, members = self.cache.get(current_file_id)
             if tar == -1:
