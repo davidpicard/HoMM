b3ef7eff917f289dbb86fdd2fa60e1167b5a3eca
David Picard <picard@ensea.fr>
2025-04-05 00:15:11 +0200

    - bigger batch size for decoding

--- src/gen_video_vbench.py
+++ src/gen_video_vbench.py
@@ -119,6 +119,6 @@ for prompt in prompt_list:
         filename = prompt[0:l]
         for index in range(5):
             # print(f"samples shape: {samples.shape}")
-            v = vae_decode_video(samples[index].squeeze().detach(), vae, batch_size=40).cpu()
+            v = vae_decode_video(samples[index].squeeze().detach(), vae, batch_size=80).cpu()
             print("writing video")
             write_video(v, f"{args.output}/{filename}-{index}.mp4", target_fps=16)
