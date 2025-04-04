72ca8658be1ad4f7c2376aaeff92722803335755
David Picard <picard@ensea.fr>
2025-04-04 23:03:46 +0200

    - fix filename length

--- src/gen_video_vbench.py
+++ src/gen_video_vbench.py
@@ -111,8 +111,10 @@ for prompt in prompt_list:
             )
 
         # sample 5 videos for each prompt
+        l = min(len(prompt, 100))
+        filename = prompt[0:l]
         for index in range(5):
             # print(f"samples shape: {samples.shape}")
-            v = vae_decode_video(samples[index].squeeze().detach(), vae, batch_size=40).cpu()
+            v = vae_decode_video(samples[index].squeeze().detach(), vae).cpu()
             print("writing video")
-            write_video(v, f"{args.output}/{prompt}-{index}.mp4", target_fps=25)
+            write_video(v, f"{args.output}/{filename}-{index}.mp4", target_fps=25)
