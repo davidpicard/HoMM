4d0fa59780db42ba1e1c57d60fb54e09d37e0bf9
David Picard <picard@ensea.fr>
2025-11-13 14:42:31 +0100

    no grad

--- src/gen_stat.py
+++ src/gen_stat.py
@@ -34,40 +34,41 @@ sig = []
 mu_s = []
 sig_s = []
 i = 1
-for b in tqdm(train):
-    x, y = b
-    img = vae.vae_decode(x.to("cuda"))
-    img = (255*img).to(torch.uint8)
-    with torch.inference_mode():
-        pred, = inception(img)
+with torch.no_grad():
+    for b in tqdm(train):
+        x, y = b
+        img = vae.vae_decode(x.to("cuda"))
+        img = (255*img).to(torch.uint8)
+        with torch.inference_mode():
+            pred, = inception(img)
 
-    pred = pred.cpu().numpy()
-    spred = spatial_features.pop().movedim(1, -1)[..., :7].cpu().numpy().reshape([pred.shape[0], -1])
-    mu.append(np.mean(pred, axis=0))
-    sig.append(np.cov(pred, rowvar=False))
-    mu_s.append(np.mean(spred, axis=0))
-    sig_s.append(np.cov(spred, rowvar=False))
-    i += 1
-    if i > 5000:
-        break
+        pred = pred.cpu().numpy()
+        spred = spatial_features.pop().movedim(1, -1)[..., :7].cpu().numpy().reshape([pred.shape[0], -1])
+        mu.append(np.mean(pred, axis=0))
+        sig.append(np.cov(pred, rowvar=False))
+        mu_s.append(np.mean(spred, axis=0))
+        sig_s.append(np.cov(spred, rowvar=False))
+        i += 1
+        if i > 5000:
+            break
 
-mu = np.stack(mu, axis=0)
-sig = np.stack(sig, axis=0)
-mu_s = np.stack(mu_s, axis=0)
-sig_s = np.stack(sig_s, axis=0)
+    mu = np.stack(mu, axis=0)
+    sig = np.stack(sig, axis=0)
+    mu_s = np.stack(mu_s, axis=0)
+    sig_s = np.stack(sig_s, axis=0)
 
-# aggregate
+    # aggregate
 
-mu = mu.mean(axis=0)
-sig = sig.sum(axis=0) * (batch_size-1) / (i*batch_size - 1)
-mu_s = mu_s.mean(axis=0)
-sig_s = sig_s.sum(axis=0)* (batch_size-1) / (i*batch_size - 1)
+    mu = mu.mean(axis=0)
+    sig = sig.sum(axis=0) * (batch_size-1) / (i*batch_size - 1)
+    mu_s = mu_s.mean(axis=0)
+    sig_s = sig_s.sum(axis=0)* (batch_size-1) / (i*batch_size - 1)
 
-print(f"mu: {mu.shape}")
-print(f"sig: {sig.shape}")
-print(f"mu_s: {mu_s.shape}")
-print(f"sig_s: {sig_s.shape}")
+    print(f"mu: {mu.shape}")
+    print(f"sig: {sig.shape}")
+    print(f"mu_s: {mu_s.shape}")
+    print(f"sig_s: {sig_s.shape}")
 
-np.savez(out, np.zeros((1000, 256, 256, 3), dtype=np.uint8), mu=mu, sigma=sig, mu_s=mu_s, sigma_s=sig_s)
+    np.savez(out, np.zeros((1000, 256, 256, 3), dtype=np.uint8), mu=mu, sigma=sig, mu_s=mu_s, sigma_s=sig_s)
 
 
